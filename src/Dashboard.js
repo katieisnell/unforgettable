1b8f6b8f3ff68988df2b4bf1e17e53f7336eed22
Katie <katie.snell@student.manchester.ac.uk>
2019-11-19 12:53:39 +0000

    Implemented routing

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -4,22 +4,23 @@ import './App.css';
 import logo from './logo.png';
 import desktopImage from './paper-desktop.jpg';
 
-class App extends React.Component {
+class Dashboard extends React.Component {
   render() {
     const imageUrl = desktopImage;
+
     return (
       <div className="App" style={{backgroundImage: `url(${imageUrl})` }}>
         <div className="App-content">
           <img src={logo} className="App-logo" alt="logo" />
           <div className='App-header'>
-            <h1>#unforgettable</h1>
-            <p>Login with <a href='http://127.0.0.1:8080/'>Instagram</a></p>
-            <p><a href='http://localhost:3000/dashboard'>Dashboard</a></p>
+            <h1>Dashboard</h1>
+            <p>Logged in</p>
           </div>
         </div>
       </div>
     );
   }
+
 }
 
-export default App;
+export default Dashboard;
