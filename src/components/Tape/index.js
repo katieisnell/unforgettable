5676981aec0d96aaea33ad4fea62c826d21938cb
Katie <katie.snell@student.manchester.ac.uk>
2020-02-17 20:56:58 +0000

    Call to Vision api to retrieve all labels

--- src/components/Tape/index.js
+++ src/components/Tape/index.js
@@ -4,7 +4,7 @@ import './Tape.css';
 import logo from '../../assets/tape.png'
 
 const Tape = (props) => (
-  <div class='image'>
+  <div className='image'>
     <img src={logo} alt={logo} width={300}/>
     <h1>{props.text}</h1>
   </div>
