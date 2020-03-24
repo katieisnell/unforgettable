2cf0563d4493aa338473a88f5f721276734fe916
Katie <katie.snell@student.manchester.ac.uk>
2020-03-24 14:28:44 +0000

    Added happy people images filter

--- src/constants/moments.js
+++ src/constants/moments.js
@@ -1,3 +1,4 @@
 export const ALL_IMAGES = 'userUploadedImages';
 export const MOST_POSTED_LABELS_IMAGES = 'most_posted_labels_images';
 export const MULTIPLE_TAGGED_PEOPLE_IMAGES = 'multiple_tagged_people_images';
+export const HAPPY_PEOPLE_IMAGES = 'happy_people_images';
